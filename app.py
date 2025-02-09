60bb139aeab5d47b48db2858b5347da22280ac86
doodle-aditya <aditya0123456@gmail.com>
2025-02-09 19:21:36 +0530

    Made contact page

--- app.py
+++ app.py
@@ -97,5 +97,9 @@ def logout():
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
