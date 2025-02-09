3d2ac7c962d4673146e8be8029d116a912f3379f
doodle-aditya <aditya0123456@gmail.com>
2025-02-09 09:32:42 +0530

    Added register and Login page and imported few library

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask,render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField,EmailField,PasswordField,SubmitField
+from wtforms.validators import DataRequired,Email,ValidationError
 
 app = Flask(__name__)
+
+class RegisterForm(FlaskForm):
+    name = StringField('Name',validators=[DataRequired()])
+    email = EmailField('Email',validators=[Email()])
+    Password = PasswordField('Password',validators=[DataRequired()])
+    Submit = SubmitField('Register')
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
